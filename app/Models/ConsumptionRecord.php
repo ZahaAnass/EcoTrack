870d01fec5bec6d7e0e671353f5445d9ead1d550
ZahaAnass <zahaanass277@gmail.com>
2025-12-09 17:55:51 +0100

    update models to useFatory

--- app/Models/ConsumptionRecord.php
+++ app/Models/ConsumptionRecord.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ConsumptionRecord extends Model
 {
-//    use HasFactory;
+    use HasFactory;
     protected $fillable = [
         'meter_id',
         'technician_id',
